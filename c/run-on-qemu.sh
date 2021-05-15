2bbeeadfb379c3ea2bfb0d68eff0695857274c19
Zongmin Lei <leizongmin@gmail.com>
2021-05-15 18:34:29 +0800

    fix

--- c/run-on-qemu.sh
+++ c/run-on-qemu.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-qemu-system-x86_64 -drive format=raw,file=target/LeiOS.bin -nographic
+qemu-system-x86_64 -drive format=raw,file=target/LeiOS.bin -nographic -m 16 -ctrl-grab -no-reboot
+# Ctrl-A X 退出
+# Ctrl-A H 帮助
