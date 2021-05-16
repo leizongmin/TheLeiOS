d0f6c01f9abb5b4ef3ffaffa5394094c557b4309
Zongmin Lei <leizongmin@gmail.com>
2021-05-16 13:00:25 +0800

    refactor: c/build-on-docker.sh

--- c/build-on-docker.sh
+++ c/build-on-docker.sh
@@ -4,9 +4,11 @@ set -e
 export current_dir=$(cd "$(dirname "$0")";pwd)
 cd "${current_dir}"
 
+export docker_run="docker run -it --rm -v "$(pwd):$(pwd)" -w "$(pwd)" --name "Build-LeiOS-$(date +%Y%m%d-%H%M%S)" leios-build"
+
 if [ -n "$1" ]
 then
-    docker run -it --rm -v "$(pwd):$(pwd)" -w "$(pwd)" --name "Build-LeiOS-$(date +%Y%m%d-%H%M%S)" leios-build $1
+    $docker_run $1
 else
-    docker run -it --rm -v "$(pwd):$(pwd)" -w "$(pwd)" --name "Build-LeiOS-$(date +%Y%m%d-%H%M%S)" leios-build ./build.sh
+    $docker_run ./build.sh
 fi
