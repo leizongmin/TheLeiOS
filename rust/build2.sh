7fdd854b954768b6996faa0337f01aa360ce567a
Zongmin Lei <leizongmin@gmail.com>
2021-05-16 12:59:10 +0800

    rust: build2.sh

---
+++ rust/build2.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+export current_dir=$(cd "$(dirname "$0")";pwd)
+cd "${current_dir}"
+
+export target_dir="${current_dir}/target"
+
+cargo build --release
+
+mkdir -p "${target_dir}/isofiles/boot/grub"
+cp grub.cfg "${target_dir}/isofiles/boot/grub/grub.cfg"
+cp "${target_dir}/leios_x86_64/release/leios" "${target_dir}/isofiles/boot/kernel.bin"
+grub-mkrescue -o "${target_dir}/LeiOS.iso" "${target_dir}/isofiles"
