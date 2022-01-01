83e27f3e69a018f6a4909beaabdda128f1f20520
Zongmin Lei <leizongmin@gmail.com>
2022-01-01 19:22:02 +0800

    feat: main

--- kernel/main.c
+++ kernel/main.c
@@ -1,25 +1,31 @@
 #include "vgastr.h"
 
 void
-main ()
+welcome ()
 {
   k_vgastr_reset (K_VGASTR_COLOR_DARK_GREY, 178);
-  k_vgastr_write_string (
-      "Like the Allman and Whitesmiths styles, GNU "
-      "style puts braces on a line by themselves, indented by two "
-      "spaces, except when opening a function definition, where "
-      "they are not indented. In either case, the contained code "
-      "is indented by two spaces from the braces.!\n");
+
+  k_vgastr_set_color (K_VGASTR_COLOR_LIGHT_BROWN);
+  for (int i = 0; i < 400; i++)
+    {
+      k_vgastr_write ('+');
+    }
+
   k_vgastr_set_color (K_VGASTR_COLOR_LIGHT_GREEN);
-  k_vgastr_write_string ("Welcome to the LeiOS! ^_^\n");
-  k_vgastr_set_color (K_VGASTR_COLOR_CYAN);
-  k_vgastr_write_string ("Going to done.\n");
-  k_vgastr_set_color (K_VGASTR_COLOR_LIGHT_RED);
+  k_vgastr_write_string ("    Welcome to the LeiOS!\n\n");
+  k_vgastr_set_color (K_VGASTR_COLOR_LIGHT_CYAN);
   k_vgastr_write_string (
-      "I/O from the guest is cached on the host but written through to the "
-      "physical medium. This mode is slower and prone to scaling problems. "
-      "Best used for small number of guests with lower I/O requirements. "
-      "Suggested for guests that do not support a writeback cache (such as "
-      "Red Hat Enterprise Linux 5.5 and earlier), where migration is not "
-      "needed.\n");
+      "    Homepage: https://github.com/leizongmin/TheLeiOS\n\n");
+
+  k_vgastr_set_color (K_VGASTR_COLOR_LIGHT_BROWN);
+  for (int i = 0; i < 400; i++)
+    {
+      k_vgastr_write ('+');
+    }
+}
+
+void
+main ()
+{
+  welcome ();
 }
