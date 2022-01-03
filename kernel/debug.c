f6a0a281559a6bd4c7c0c3ce33be6cc35a9cdd19
Zongmin Lei <leizongmin@gmail.com>
2022-01-03 23:17:59 +0800

    feat: 优化 debug

--- kernel/debug.c
+++ kernel/debug.c
@@ -5,51 +5,52 @@
 void k_debug_assert(const char* _func, const char* _file, usize _line,
                     const char* msg, bool condition) {
   if (!condition) {
-    DEBUG_KEEP_VGASTR_STATUS(DEBUG_MESSAGE_DEBUG_COLOR, {
-      k_vgastr_printf("== ASSERT FAILURE: [%s] AT %s(%s:%d) ==\n", msg, _func,
-                      _file, _line);
+    DEBUG_KEEP_CURRENT_OUTPUT_STATUS(DEBUG_MESSAGE_DEBUG_COLOR, {
+      DEBUG_FUNCTION_WRITE_FORMAT("== ASSERT FAILURE: [%s]\n== AT %s(%s:%d)\n",
+                                  msg, _func, _file, _line);
     })
   }
 }
 
 void k_debug_str(const char* _func, const char* _file, usize _line,
                  const char* prefix, const char* s) {
-  DEBUG_KEEP_VGASTR_STATUS(DEBUG_MESSAGE_DEBUG_COLOR, {
-    k_vgastr_printf("== %s: %s AT %s(%s:%d) ==\n", prefix, s, _func, _file,
-                    _line);
+  DEBUG_KEEP_CURRENT_OUTPUT_STATUS(DEBUG_MESSAGE_DEBUG_COLOR, {
+    DEBUG_FUNCTION_WRITE_FORMAT("== %s: %s\n== AT %s(%s:%d)\n", prefix, s,
+                                _func, _file, _line);
   })
 }
 
 void k_debug_error(const char* _func, const char* _file, usize _line,
                    const char* prefix, const char* s) {
-  DEBUG_KEEP_VGASTR_STATUS(DEBUG_MESSAGE_ERROR_COLOR, {
-    k_vgastr_printf("== %s: %s AT %s(%s:%d) ==\n", prefix, s, _func, _file,
-                    _line);
+  DEBUG_KEEP_CURRENT_OUTPUT_STATUS(DEBUG_MESSAGE_ERROR_COLOR, {
+    DEBUG_FUNCTION_WRITE_FORMAT("== %s: %s\n== AT %s(%s:%d)\n", prefix, s,
+                                _func, _file, _line);
   })
 }
 
 void k_debug_int(const char* _func, const char* _file, usize _line,
                  const char* prefix, int n) {
-  DEBUG_KEEP_VGASTR_STATUS(DEBUG_MESSAGE_DEBUG_COLOR, {
-    k_vgastr_printf("== %s: 0x%d AT %s(%s:%d) ==\n", prefix, n, _func, _file,
-                    _line);
+  DEBUG_KEEP_CURRENT_OUTPUT_STATUS(DEBUG_MESSAGE_DEBUG_COLOR, {
+    DEBUG_FUNCTION_WRITE_FORMAT("== %s: 0x%d\n== AT %s(%s:%d)\n", prefix, n,
+                                _func, _file, _line);
   })
 }
 
 void k_debug_dump_ptr(const char* _func, const char* _file, usize _line,
                       const void* ptr, usize n) {
-  DEBUG_KEEP_VGASTR_STATUS(DEBUG_MESSAGE_DEBUG_COLOR, {
+  DEBUG_KEEP_CURRENT_OUTPUT_STATUS(DEBUG_MESSAGE_DEBUG_COLOR, {
     u8* p = (u8*)ptr;
-    k_vgastr_printf("== DUMP DATA FROM POINTER [0x%x] AT %s(%s:%d) ==\n", _func,
-                    _file, _line, p);
+    DEBUG_FUNCTION_WRITE_FORMAT(
+        "== DUMP DATA FROM POINTER [0x%x]\n== AT %s(%s:%d)\n", p, _func, _file,
+        _line);
     for (usize i = 0; i < n; i++) {
-      k_vgastr_printf("0x%x", *p++);
+      DEBUG_FUNCTION_WRITE_FORMAT("0x%x", *p++);
       if (i > 0 && i % 16 == 0) {
-        k_vgastr_write('\n');
+        DEBUG_FUNCTION_WRITE_CHAR('\n');
       } else {
-        k_vgastr_write(' ');
+        DEBUG_FUNCTION_WRITE_CHAR(' ');
       }
     }
-    k_vgastr_write_str("\n== DUMP DATA END ==\n");
+    DEBUG_FUNCTION_WRITE_STR("\n== DUMP DATA END ==\n");
   })
 }
--- kernel/debug.h
+++ kernel/debug.h
@@ -5,6 +5,15 @@
 
 #include "vgastr.h"
 
+// if there are many debug output place,
+// define the function and variable name here
+#define DEBUG_FUNCTION_WRITE_CHAR(c) k_vgastr_write(c)
+#define DEBUG_FUNCTION_WRITE_STR(s) k_vgastr_write_str(s)
+#define DEBUG_FUNCTION_WRITE_FORMAT(fmt, ...) k_vgastr_printf(fmt, __VA_ARGS__)
+#define DEBUG_FUNCTION_COLOR k_vgastr_color
+#define DEBUG_FUNCTION_COLOR_SET(c) k_vgastr_set_color(c)
+#define DEBUG_FUNCTION_OFFSET_COLUMN k_vgastr_offset_column
+
 void k_debug_assert(const char* _func, const char* _file, usize line,
                     const char* msg, bool condition);
 void k_debug_str(const char* _func, const char* _file, usize line,
@@ -24,15 +33,15 @@ void k_debug_error(const char* _func, const char* _file, usize _line,
   (K_VGASTR_COLOR_LIGHT_RED + K_VGASTR_BGCOLOR_DARK_GREY)
 #define DEBUG_MESSAGE_ERROR_PREFIX "ERROR"
 
-#define DEBUG_KEEP_VGASTR_STATUS(c, x) \
-  {                                    \
-    u8 color = k_vgastr_color;         \
-    k_vgastr_set_color(c);             \
-    if (k_vgastr_offset_column > 0) {  \
-      k_vgastr_write('\n');            \
-    }                                  \
-    x;                                 \
-    k_vgastr_set_color(color);         \
+#define DEBUG_KEEP_CURRENT_OUTPUT_STATUS(c, x) \
+  {                                            \
+    u8 color = DEBUG_FUNCTION_COLOR;           \
+    DEBUG_FUNCTION_COLOR_SET(c);               \
+    if (DEBUG_FUNCTION_OFFSET_COLUMN > 0) {    \
+      DEBUG_FUNCTION_WRITE_CHAR('\n');         \
+    }                                          \
+    x;                                         \
+    DEBUG_FUNCTION_COLOR_SET(color);           \
   }
 
 #ifdef NDEBUG
@@ -51,11 +60,12 @@ void k_debug_error(const char* _func, const char* _file, usize _line,
   k_debug_str(__FUNCTION__, __FILE__, __LINE__, DEBUG_MESSAGE_DEBUG_PREFIX, s)
 #define DEBUG_INT(n) \
   k_debug_int(__FUNCTION__, __FILE__, __LINE__, DEBUG_MESSAGE_DEBUG_PREFIX, n)
-#define DEBUG_PRINTF(fmt, ...)                                               \
-  DEBUG_KEEP_VGASTR_STATUS(DEBUG_MESSAGE_DEBUG_COLOR, {                      \
-    k_vgastr_printf("== %s ", DEBUG_MESSAGE_DEBUG_PREFIX);                   \
-    k_vgastr_printf(fmt, __VA_ARGS__);                                       \
-    k_vgastr_printf(" AT %s(%s:%d) ==\n", __FUNCTION__, __FILE__, __LINE__); \
+#define DEBUG_PRINTF(fmt, ...)                                                 \
+  DEBUG_KEEP_CURRENT_OUTPUT_STATUS(DEBUG_MESSAGE_DEBUG_COLOR, {                \
+    DEBUG_FUNCTION_WRITE_FORMAT("== %s ", DEBUG_MESSAGE_DEBUG_PREFIX);         \
+    DEBUG_FUNCTION_WRITE_FORMAT(fmt, __VA_ARGS__);                             \
+    DEBUG_FUNCTION_WRITE_FORMAT("\n== AT %s(%s:%d)\n", __FUNCTION__, __FILE__, \
+                                __LINE__);                                     \
   })
 #define DEBUG_DUMP_PTR(ptr, n) \
   k_debug_dump_ptr(__FUNCTION__, __FILE__, __LINE__, ptr, n)
@@ -65,11 +75,12 @@ void k_debug_error(const char* _func, const char* _file, usize _line,
 #define DEBUG_UNUSED(x) (void)(x)
 #define DEBUG_ERROR(s) \
   k_debug_error(__FUNCTION__, __FILE__, __LINE__, DEBUG_MESSAGE_ERROR_PREFIX, s)
-#define DEBUG_ERRORF(fmt, ...)                                               \
-  DEBUG_KEEP_VGASTR_STATUS(DEBUG_MESSAGE_ERROR_COLOR, {                      \
-    k_vgastr_printf("== %s ", DEBUG_MESSAGE_ERROR_PREFIX);                   \
-    k_vgastr_printf(fmt, __VA_ARGS__);                                       \
-    k_vgastr_printf(" AT %s(%s:%d) ==\n", __FUNCTION__, __FILE__, __LINE__); \
+#define DEBUG_ERRORF(fmt, ...)                                                 \
+  DEBUG_KEEP_CURRENT_OUTPUT_STATUS(DEBUG_MESSAGE_ERROR_COLOR, {                \
+    DEBUG_FUNCTION_WRITE_FORMAT("== %s ", DEBUG_MESSAGE_ERROR_PREFIX);         \
+    DEBUG_FUNCTION_WRITE_FORMAT(fmt, __VA_ARGS__);                             \
+    DEBUG_FUNCTION_WRITE_FORMAT("\n== AT %s(%s:%d)\n", __FUNCTION__, __FILE__, \
+                                __LINE__);                                     \
   })
 
 #endif  //_KERNEL_DEBUG_H_
